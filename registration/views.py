dfaa421cbd1f629195a3b66f8fb41abb6921c95f
vpasbar <microedet@microedet.com>
2021-10-08 19:19:40 +0200

    se comprueba el email unico

--- registration/views.py
+++ registration/views.py
@@ -1,14 +1,13 @@
-
-from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import UserCreationEmail
 from django.views.generic import CreateView
 from django.urls import  reverse_lazy
-from django import forms
 
 # Create your views here.
 class SigUpView(CreateView):
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
-    form_class = UserCreationForm
+    form_class = UserCreationEmail
 
     def get_success_url(self):
         return reverse_lazy('login')+'?register'
@@ -17,6 +16,7 @@ class SigUpView(CreateView):
         form = super(SigUpView,self).get_form()
         #modifico los campos en tiempo real
         form.fields['username'].widget = forms.TextInput(attrs={'class':'form-control mb-2', 'placeholder':'Nombre de Usuario'})
+        form.fields['email'].widget = forms.EmailInput(attrs={'class':'form-control mb-2', 'placeholder':'Email'})
         form.fields['password1'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2', 'placeholder':'Contraseña'})
         form.fields['password2'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2', 'placeholder':'Confirmar Contraseña'})
         return form
